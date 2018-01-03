87683c0eb473675dc0cb7a5cee0e4e3510cac609
gabriela <lisbga42@gmail.com>
2018-01-02 23:01:12 -0500

    lab completed

---
+++ src/main/resources/script.sql
@@ -0,0 +1,26 @@
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES('Howard the Duck', '110', 'Sci-Fi', '4.6', 'PG'),
+('Lavalantula', '83', 'Horror', '4.7', 'TV-14'),
+('Starship Troopers', '129', 'Sci-Fi', '7.2', 'PG-13'),
+('Waltz With Bashir', '90', 'Documentary', '8.0', 'R'),
+('Spaceballs', '96', 'Comedy', '7.1', 'PG'),
+('Monsters Inc.', '92', 'Animation', '8.1','G'),
+('The Room', '100', 'Drama', '3.6', 'R'),
+('The Princess Bride', '120', 'Adventure', '8.1', 'PG'),
+('The Rocky Horror Picture Show', '130', 'Comedy', '7.4', 'R');
+
+SELECT * FROM movies WHERE genre='Sci-Fi';
+SELECT * FROM movies WHERE imdb_score >= 6.5;
+SELECT * FROM movies WHERE runtime<=100 AND rating='PG' OR rating='G';
+SELECT genre, AVG(runtime) FROM movies WHERE imdb_score <= 7.5 GROUP BY genre;
+UPDATE movies SET rating = 'R' WHERE title = 'Starship Troopers';
+SELECT id, genre, rating FROM movies WHERE genre = 'Documentary' OR genre = 'Horror';
+SELECT rating, AVG(imdb_score), MIN(imdb_score), MAX(imdb_score) FROM movies GROUP BY rating;
+SELECT rating, AVG(imdb_score), MIN(imdb_score), MAX(imdb_score) FROM movies GROUP BY rating  HAVING COUNT(*) > 1;
+DELETE FROM movies WHERE rating='R';
+
+
+
+
+
+
